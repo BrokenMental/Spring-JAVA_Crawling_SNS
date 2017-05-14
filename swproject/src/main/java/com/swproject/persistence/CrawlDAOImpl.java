bdd635acf6d5f9107e8eec69a88477a17cacb216
Nuka <commontoday@naver.com>
2017-05-15 04:19:05 +0900

    CrawlDAOImpl에 @Repository 어노테이션을 추가해주니까 404가 안뜬다.

--- swproject/src/main/java/com/swproject/persistence/CrawlDAOImpl.java
+++ swproject/src/main/java/com/swproject/persistence/CrawlDAOImpl.java
@@ -3,9 +3,11 @@ package com.swproject.persistence;
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
 
 import com.swproject.domain.CrawlerVO;
 
+@Repository
 public class CrawlDAOImpl implements CrawlDAO {
 	
 	@Inject

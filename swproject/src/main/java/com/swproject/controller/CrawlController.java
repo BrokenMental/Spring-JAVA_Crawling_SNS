7f818c8f2e0509eb117b5a96393e4905188aa815
Nuka <commontoday@naver.com>
2017-06-07 00:04:19 +0900

    controller에서 service로 기능구현 옮김

--- swproject/src/main/java/com/swproject/controller/CrawlController.java
+++ swproject/src/main/java/com/swproject/controller/CrawlController.java
@@ -36,66 +36,16 @@ public class CrawlController {
 	private CrawlService service;
 	
 	@RequestMapping(value = "/CrawlList", method = {RequestMethod.GET, RequestMethod.POST})
-	public void CrawlPage1(@ModelAttribute("cri") SearchCriteria cri, @ModelAttribute("el") Elements el, CrawlerVO Crawl, Model model) throws Exception {
-		CrawlerNews Craw = new CrawlerNews();
-		Craw.setURL("https://news.google.co.kr");
-		Craw.setDoc(Jsoup.connect(Craw.getURL()).get());
-		Craw.setEl(Craw.getDoc().select("div.esc-lead-article-title-wrapper a"));
-		
-		/*Document ImageL = Jsoup.connect("https://news.google.co.kr").get();
-		Elements ImageLink = ImageL.select("img.esc-thumbnail-image");
-		String ILink = ImageLink.attr("src").toString();
-		model.addAttribute("LTest",ILink);*/
-		
-		java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat("yyyy.MM.dd. HH:mm:ss"); // yyyy.MM.dd. HH:mm:ss
-		String now = formatter.format(new java.util.Date());
-		Crawl.setC_Time(now);
-		
-		el = Craw.getEl();
+	public void CrawlPage1(CrawlerVO Crawl, Model model) throws Exception {
 
-		for (Element temp : el) { // 페이지에 뿌려주는것과 DB에 넣어주는것을 따로 해야한다.
-			Crawl.setC_Group("News");
-			Crawl.setN_Title(temp.text().toString());
-			Crawl.setURL(temp.attr("href").toString());
-
-			service.create1(Crawl);
-		}
-
-		System.out.println(Crawl);
 		model.addAttribute("list1",service.listCrawl1(Crawl));
 		
 		logger.info("CrawlPage1 .........");
-		
-		// �럹�씠吏� 泥섎━
-		/*PageMaker pageMaker = new PageMaker();
-		pageMaker.setCri(cri);
-
-		pageMaker.setTotalCount(service.listSearchCount(cri));
-
-		model.addAttribute("pageMaker", pageMaker);*/
 
 	}
 
 	@RequestMapping(value = "/CrawlList2", method = {RequestMethod.GET, RequestMethod.POST})
 	public void CrawlPage2(CrawlerVO Crawl, Model model) throws Exception {
-		CrawlerSNS CS = new CrawlerSNS();
-		CS.setTwitter(TwitterFactory.getSingleton());
-		Twitter gt = CS.getTwitter();
-		CS.setUser(gt.verifyCredentials());
-		CS.setList(gt.getHomeTimeline());
-		List<Status> cl = CS.getList();
-
-		java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat("yyyy.MM.dd. HH:mm:ss"); // yyyy.MM.dd. HH:mm:ss
-		String now = formatter.format(new java.util.Date());
-		Crawl.setC_Time(now);
-
-		for (Status temp1 : cl) {
-			Crawl.setC_Group("SNS");
-			Crawl.setS_User(temp1.getUser().getScreenName().toString());
-			Crawl.setS_Content(temp1.getText().toString());
-
-			service.create2(Crawl);
-		}
 
 		model.addAttribute("list2",service.listCrawl2(Crawl));
 
--- swproject/src/main/java/com/swproject/service/CrawlServiceImpl.java
+++ swproject/src/main/java/com/swproject/service/CrawlServiceImpl.java
@@ -4,17 +4,36 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
 import org.springframework.stereotype.Service;
 
+import com.swproject.domain.CrawlerNews;
+import com.swproject.domain.CrawlerSNS;
 import com.swproject.domain.CrawlerVO;
 import com.swproject.domain.SearchCriteria;
 import com.swproject.persistence.CrawlDAO;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
 
 @Service
 public class CrawlServiceImpl implements CrawlService {
 	
 	@Inject
 	private CrawlDAO dao;
+
+	@Inject
+	private CrawlService service;
+	
+	public String time(){
+
+		java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat("yyyy.MM.dd. HH:mm:ss"); // yyyy.MM.dd. HH:mm:ss
+		String now = formatter.format(new java.util.Date());
+		return now;
+		
+	}
 	
 	@Override
 	public void create1(CrawlerVO crawl) throws Exception{
@@ -28,11 +47,43 @@ public class CrawlServiceImpl implements CrawlService {
 	
 	@Override
 	public List<CrawlerVO> listCrawl1(CrawlerVO cn) throws Exception{
+		CrawlerNews Craw = new CrawlerNews();
+		Craw.setURL("https://news.google.co.kr");
+		Craw.setDoc(Jsoup.connect(Craw.getURL()).get());
+		Craw.setEl(Craw.getDoc().select("div.esc-lead-article-title-wrapper a"));
+		
+		cn.setC_Time(time());
+
+		for (Element temp : Craw.getEl()) { // 페이지에 뿌려주는것과 DB에 넣어주는것을 따로 해야한다.
+			cn.setC_Group("News");
+			cn.setN_Title(temp.text().toString());
+			cn.setURL(temp.attr("href").toString());
+
+			service.create1(cn);
+		}
+
+		System.out.println(cn);
 		return dao.listCrawl1(cn);
 	}
 
 	@Override
 	public List<CrawlerVO> listCrawl2(CrawlerVO sns) throws Exception{
+		CrawlerSNS CS = new CrawlerSNS();
+		CS.setTwitter(TwitterFactory.getSingleton());
+		Twitter gt = CS.getTwitter();
+		CS.setUser(gt.verifyCredentials());
+		CS.setList(gt.getHomeTimeline());
+		List<Status> cl = CS.getList();
+
+		sns.setC_Time(time());
+
+		for (Status temp1 : cl) {
+			sns.setC_Group("SNS");
+			sns.setS_User(temp1.getUser().getScreenName().toString());
+			sns.setS_Content(temp1.getText().toString());
+
+			service.create2(sns);
+		}
 		return dao.listCrawl2(sns);
 	}
 	

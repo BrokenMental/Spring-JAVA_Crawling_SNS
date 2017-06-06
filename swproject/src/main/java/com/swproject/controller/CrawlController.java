d517195552fa6e7886ec565349039bf9f05118d8
Nuka <commontoday@naver.com>
2017-06-07 00:04:47 +0900

    import문 정리

--- swproject/src/main/java/com/swproject/controller/CrawlController.java
+++ swproject/src/main/java/com/swproject/controller/CrawlController.java
@@ -1,31 +1,16 @@
 package com.swproject.controller;
 
-import java.util.List;
-
 import javax.inject.Inject;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.swproject.domain.CrawlerNews;
-import com.swproject.domain.CrawlerSNS;
 import com.swproject.domain.CrawlerVO;
-import com.swproject.domain.PageMaker;
-import com.swproject.domain.SearchCriteria;
 import com.swproject.service.CrawlService;
-
-import twitter4j.Status;
-import twitter4j.Twitter;
-import twitter4j.TwitterFactory;
 
 @Controller
 @RequestMapping("/Crawl/*")

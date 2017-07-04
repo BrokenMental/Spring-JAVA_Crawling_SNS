6eb7973283c266ba2b8a59f267e166d5c6b926e4
Jinuk <commontoday@naver.com>
2017-07-04 18:03:30 +0900

    오류 수정하자

--- swproject/src/main/java/com/swproject/service/CrawlServiceImpl.java
+++ swproject/src/main/java/com/swproject/service/CrawlServiceImpl.java
@@ -21,69 +21,81 @@ import twitter4j.TwitterFactory;
 
 @Service
 public class CrawlServiceImpl implements CrawlService {
-	
+
 	@Inject
 	private CrawlDAO dao;
 
 	@Inject
 	private CrawlService service;
-	
-	public String time(){
 
-		java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat("yyyy.MM.dd. HH:mm:ss"); // yyyy.MM.dd. HH:mm:ss
+	public String time() {
+
+		java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat("yyyy.MM.dd. HH:mm:ss"); // yyyy.MM.dd.
+																										// HH:mm:ss
 		String now = formatter.format(new java.util.Date());
 		return now;
-		
+
 	}
-	
-	public void split(CrawlerVO sns, Status temp1){
+
+	public void split(CrawlerVO sns, Status temp1) {
 		String crawl = temp1.getText().toString();
 		String content, addr;
 		int i = 0;
-		if(crawl.indexOf("http") != -1){
+		if (crawl.indexOf("http") != -1) {
 			i = crawl.indexOf("http");
-			content = crawl.substring(0,i);
-			addr = crawl.substring(i+1);
-			
+			content = crawl.substring(0, i);
+			addr = crawl.substring(i + 1);
+
 			sns.setS_Content(content);
-			sns.setS_Addr('h'+addr);
+			sns.setS_Addr('h' + addr);
 		}
 	}
-	
+
 	@Override
-	public void create1(CrawlerVO crawl) throws Exception{
+	public void create1(CrawlerVO crawl) throws Exception {
 		dao.create1(crawl);
 	}
-	
+
 	@Override
-	public void create2(CrawlerVO crawl) throws Exception{
+	public void create2(CrawlerVO crawl) throws Exception {
 		dao.create2(crawl);
 	}
-	
+
 	@Override
-	public List<CrawlerVO> listCrawl1(CrawlerVO cn) throws Exception{
+	public List<CrawlerVO> listCrawl1(CrawlerVO cn) throws Exception {
 		CrawlerNews Craw = new CrawlerNews();
-		Craw.setURL("https://news.google.co.kr/news/?ned=kr"); // 뒤에 부가적인 kr을 붙이지 않으면 영문페이지를 끌고온다.
+		Craw.setURL("https://news.google.co.kr/news/?ned=kr"); // 뒤에 부가적인 kr을
+																// 붙이지 않으면
+																// 영문페이지를 끌고온다.
 		Craw.setDoc(Jsoup.connect(Craw.getURL()).get());
 		Craw.setEl(Craw.getDoc().select("c-wiz.PaqQNc"));
 
 		for (Element temp : Craw.getEl()) {
 			cn.setC_Group("News");
 			cn.setN_IMG(temp.select("div.X20oP img").attr("src").toString());
-			cn.setN_Source(temp.select("span.IH8C7b Pc0Wt").text());
+			
+			// 추가 뉴스제공사가 추가되니 뒤에껀 잘라주자
+			String sp = temp.select("span.IH8C7b").text();
+			// 띄어쓰기가 없을경우 자르지말아야 한다.
+			int index = sp.indexOf(' ') + 1;
+			String Source = sp.substring(index, sp.length() - index);
+			cn.setN_Source(Source);
+			
+			//cn.setN_Source(temp.select("span.IH8C7b").text());
+			System.out.println(cn.getN_Source());
+			
 			cn.setN_Title(temp.select("a.nuEeue").text().toString());
 			cn.setURL(temp.select("a.nuEeue").attr("href").toString());
 			cn.setC_Time(time());
-			
+
 			service.create1(cn);
 		}
 
-		System.out.println(cn);
 		return dao.listCrawl1(cn);
 	}
 
 	@Override
-	public List<CrawlerVO> listCrawl2(CrawlerVO sns) throws Exception{
+	public List<CrawlerVO> listCrawl2(CrawlerVO sns) throws Exception {
 		CrawlerSNS CS = new CrawlerSNS();
 		CS.setTwitter(TwitterFactory.getSingleton());
 		Twitter gt = CS.getTwitter();
@@ -101,7 +113,7 @@ public class CrawlServiceImpl implements CrawlService {
 		}
 		return dao.listCrawl2(sns);
 	}
-	
+
 	@Override
 	public List<CrawlerVO> listSearchCriteria(SearchCriteria cri) throws Exception {
 		return dao.listSearch(cri);
@@ -111,5 +123,5 @@ public class CrawlServiceImpl implements CrawlService {
 	public int listSearchCount(SearchCriteria cri) throws Exception {
 		return dao.listSearchCount(cri);
 	}
-	
+
 }

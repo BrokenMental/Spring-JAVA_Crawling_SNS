df4f708eed7765ae4b2fb5d6c17c728d91a59936
Jinuk <commontoday@naver.com>
2018-03-13 17:40:05 +0900

    log파일 수정 및 sql문 추가

---
+++ swproject/SQL/crawler.sql
@@ -0,0 +1,13 @@
+create table crawler (
+	c_number int auto_increment,
+	c_group varchar(20),
+	n_title varchar(300),
+	url varchar(500),
+	s_content varchar(2000),
+	s_user varchar(20),
+	c_time datetime not null,
+	s_addr varchar(500),
+	n_img varchar(5000),
+	n_source varchar(50),
+	primary key(c_number)
+);

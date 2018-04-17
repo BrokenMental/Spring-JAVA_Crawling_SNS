7a880339fd0149bff6e814fc40534f95c065d484
Jinuk <commontoday@naver.com>
2018-04-17 14:54:12 +0900

    pull merge

--- swproject/SQL/crawler.sql
+++ swproject/SQL/crawler.sql
@@ -3,13 +3,13 @@ create table crawler (
 	c_group varchar(20),
 	n_title varchar(300),
 	url varchar(500),
-	s_content varchar(2000),
+	s_content varchar(5000),
 	s_user varchar(20),
 	c_time datetime not null,
 	s_addr varchar(500),
 	n_img varchar(5000),
 	n_source varchar(50),
 	primary key(c_number)
-);
+) character set utf8mb4 collate utf8mb4_unicode_ci;
 
 alter database swproject character set=utf8mb4 collate utf8mb4_unicode_ci;
